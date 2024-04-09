ba2db147ad65f8cc157a3d6cfd5dc24f3a7dcef5
SCY <helenason@naver.com>
2024-04-09 18:44:35 +0900

    feat(0409): priority queue

---
+++ baekjoon/B1927.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class B1927 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            if (!input.equals("0")) {
+                pq.offer(Integer.parseInt(input));
+                continue;
+            }
+            if (pq.isEmpty()) {
+                sb.append("0\n");
+                continue;
+            }
+            sb.append(pq.poll());
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
