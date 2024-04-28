ef210ffca81642b3657841ad4fcfc2b2d1b691f3
SCY <helenason@naver.com>
2024-04-28 18:47:14 +0900

    refactor(0428): B7576

--- baekjoon/B7576.java
+++ baekjoon/B7576.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class B7576 {
 
     static int[][] graph;
-    static int count;
+    static List<Position> queue;
+    static int count = 0;
+    static int notDone = 0;
     static int M;
     static int N;
 
@@ -20,50 +23,49 @@ public class B7576 {
         N = Integer.parseInt(input[1]);
         graph = new int[N][M];
 
-        List<Position> startPositions = new ArrayList<>();
+        // initialize
+        queue = new LinkedList<>();
         for (int i = 0; i < N; i++) {
             String[] row = br.readLine().split(" "); // M개
             for (int j = 0; j < M; j++) {
                 int value = Integer.parseInt(row[j]);
                 graph[i][j] = value;
+                if (value == 0) {
+                    notDone++;
+                }
                 if (value == 1) {
-                    startPositions.add(new Position(i, j));
+                    queue.add(new Position(i, j));
                 }
             }
         }
 
-        move(startPositions);
+        move();
 
-        for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                if (graph[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
-            }
+        if (notDone > 0) {
+            System.out.println(-1);
+            return;
         }
         System.out.println(count);
     }
 
-    static void move(List<Position> starts) {
+    static void move() {
         int[] dx = new int[]{0, 0, -1, 1};
         int[] dy = new int[]{-1, 1, 0, 0};
 
-        List<Position> nows = starts;
-
-        while (!nows.isEmpty()) {
+        while (!queue.isEmpty()) {
             List<Position> nexts = new ArrayList<>();
-            for (Position now : nows) {
+            for (Position now : queue) {
                 for (int i = 0; i < 4; i++) {
                     int nx = now.x + dx[i];
                     int ny = now.y + dy[i];
                     if (nx >= 0 && nx < N && ny >= 0 && ny < M && graph[nx][ny] == 0) {
                         graph[nx][ny] = 1;
                         nexts.add(new Position(nx, ny));
+                        notDone--;
                     }
                 }
             }
-            nows = nexts;
+            queue = nexts;
             if (!nexts.isEmpty()) {
                 count++;
             }
@@ -73,6 +75,7 @@ public class B7576 {
     static class Position {
         int x;
         int y;
+
         public Position(int x, int y) {
             this.x = x;
             this.y = y;
