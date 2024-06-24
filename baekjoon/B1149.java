e5664bb9b2248e06ca0ad62e41c309a885f3531b
SCY <helenason@naver.com>
2024-06-24 18:24:37 +0900

    refactor(0624): B1149 - short version

--- baekjoon/B1149.java
+++ baekjoon/B1149.java
@@ -17,15 +17,9 @@ public class B1149 {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
                 dp[i][j] = Integer.parseInt(st.nextToken(" "));
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < 3; j++) {
                 dp[i][j] += Math.min(dp[i - 1][(j + 1) % 3], dp[i - 1][(j + 2) % 3]);
             }
         }
-
         System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
     }
 }
