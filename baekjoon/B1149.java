5b6e9054308d6a286514c6e2061430274d4d21c1
SCY <helenason@naver.com>
2024-06-24 18:19:37 +0900

    feat(0624): dynamic programming

---
+++ baekjoon/B1149.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1149 {
+
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1][3];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken(" "));
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] += Math.min(dp[i - 1][(j + 1) % 3], dp[i - 1][(j + 2) % 3]);
+            }
+        }
+
+        System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
+    }
+}
